a845423eb1b4034c35eb255d64e98a6153bd4041
zhufengnodejs <zhufengnodejs@126.com>
2017-05-23 11:01:07 +0800

    3.定义状态并传入MessageList组件

--- src/MessageBox.js
+++ src/MessageBox.js
@@ -3,7 +3,15 @@ import 'bootstrap/dist/css/bootstrap.css'
 import MessageHeader from './MessageHeader';
 import MessageList from './MessageList';
 import MessageForm from './MessageForm';
+
 class MessageBox extends Component {
+  constructor(props){
+     super(props);
+     //初始化状态对象
+     this.state = {messages:[
+         {id:1,name:'张三',content:'今天天气不错!!!',createAt:new Date()}
+     ]};
+  }
   render() {
     return (
       <div className="panel panel-default">
@@ -11,7 +19,7 @@ class MessageBox extends Component {
             <MessageHeader/>
         </div>
         <div className="panel-body">
-            <MessageList/>
+            <MessageList messages={this.state.messages}/>
         </div>
         <div className="panel-footer">
             <MessageForm/>
--- src/MessageList.js
+++ src/MessageList.js
@@ -3,9 +3,14 @@ export default class MessageList extends React.Component{
     render(){
         return (
             <ul className="list-group">
-                <li className="list-group-item">
-                    张三:今天下雨  <span className="pull-right">2017年5月23日10:07:59</span>
-                </li>
+                {
+                    this.props.messages.map((message,index)=>(
+                        <li className="list-group-item">
+                            {message.name}:{message.content}  <span className="pull-right">{message.createAt.toLocaleString()}</span>
+                        </li>
+                    ))
+                }
+
             </ul>
         )
     }
