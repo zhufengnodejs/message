e6f3b724ceb21eea7c1a54629fed2ddbde8e0b00
zhufengnodejs <zhufengnodejs@126.com>
2017-05-23 11:18:16 +0800

    4.实现添加留言的功能

--- src/MessageBox.js
+++ src/MessageBox.js
@@ -1,32 +1,45 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.css'
 import MessageHeader from './MessageHeader';
 import MessageList from './MessageList';
 import MessageForm from './MessageForm';
 
 class MessageBox extends Component {
-  constructor(props){
-     super(props);
-     //初始化状态对象
-     this.state = {messages:[
-         {id:1,name:'张三',content:'今天天气不错!!!',createAt:new Date()}
-     ]};
-  }
-  render() {
-    return (
-      <div className="panel panel-default">
-        <div className="panel-heading">
-            <MessageHeader/>
-        </div>
-        <div className="panel-body">
-            <MessageList messages={this.state.messages}/>
-        </div>
-        <div className="panel-footer">
-            <MessageForm/>
-        </div>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        //初始化状态对象
+        this.state = {
+            messages: [
+                {id: 1, name: '张三', content: '今天天气不错!!!', createAt: new Date()}
+            ]
+        };
+    }
+
+    addMessage = (message) => {
+        let messages = this.state.messages;
+        message.id = messages.length > 0 ? messages[messages.length - 1].id + 1 : 1;
+        message.createAt = new Date();
+        messages.push(message)
+        this.setState({
+            messages
+        });
+    }
+
+    render() {
+        return (
+            <div className="panel panel-default">
+                <div className="panel-heading">
+                    <MessageHeader/>
+                </div>
+                <div className="panel-body">
+                    <MessageList messages={this.state.messages}/>
+                </div>
+                <div className="panel-footer">
+                    <MessageForm addMessage={this.addMessage}/>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default MessageBox;
--- src/MessageForm.js
+++ src/MessageForm.js
@@ -1,15 +1,23 @@
 import React from 'react'
 export  default class MessageForm extends React.Component{
+    //事件处理函数
+    handleSubmit = (event)=>{
+        event.preventDefault();//阻止默认表单提交事件，如果没有这句话表单页面会进行刷新
+        let name = this.name.value;
+        let content = this.content.value;
+        this.props.addMessage({name,content})
+        this.content.value = '';
+    }
     render(){
         return (
-            <form>
+            <form onSubmit={this.handleSubmit}>
                 <div className="form-group">
                     <label htmlFor="name">姓名</label>
-                    <input type="text" className="form-control" id="name" placeholder="姓名"/>
+                    <input type="text" className="form-control" ref={ref=>this.name= ref} id="name" placeholder="姓名"/>
                 </div>
                 <div className="form-group">
                     <label htmlFor="content">内容</label>
-                    <textarea className="form-control" id="content" cols="30" rows="10"></textarea>
+                    <textarea className="form-control" id="content" cols="30" rows="10"  ref={ref=>this.content = ref}></textarea>
                 </div>
                 <div className="container">
                     <button type="submit" className="btn btn-primary">提交</button>
